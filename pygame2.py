c9505e6c9ed1c9d91845052067b961f2fcebea23
moosemaniam <vikasmk@gmail.com>
2014-02-19 21:53:49 +0530

    bat made larger. Better collission detection. ball reset added

--- pygame2.py
+++ pygame2.py
@@ -2,9 +2,10 @@ import pygame
 BLACK=(0,0,0)
 WHITE=(255,255,255)
 WINNING_SCORE=20
+import random
 
 def display_box(screen, message,pos):
-        fontobject=pygame.font.SysFont('Arial', 12)
+        fontobject=pygame.font.SysFont('Arial', 40)
         if len(message) != 0:
             screen.blit(fontobject.render(message, 1, (255, 255, 255)),
                     (pos))
@@ -43,6 +44,16 @@ class ball(object):
      self.x_pos += self.x_sign * self.speedx
      self.y_pos += self.y_sign * self.speedy
      pos = (self.x_pos,self.y_pos)
+   def reset(self):
+     w = self.screen.get_width()
+     h = self.screen.get_height()
+     self.x_pos = random.randint(w/2-30,w/2+30)
+     self.y_pos = random.randint(h/2-30,h/2+30)
+     self.speedx = 5
+     self.speedy = 5
+     self.x_sign *= -1
+     self.y_sign *= -1
+     self.draw()
    
 class bat(object):
    offset=5
@@ -90,13 +101,8 @@ class Pong(object):
         screen_rect = pygame.Rect(0,0,screen.get_width(),screen.get_height())
    #boundaries
    #def __init__(self,left,top,width,height,color):
-        top = boundary(-20,-20,wsize,5+20,WHITE)
-        bottom= boundary(0,hsize-5,wsize,5+20,WHITE)
-        left= boundary(-20,-20,5+20,hsize,WHITE)
-        right= boundary(wsize-5,0,5+20,hsize,WHITE)
-        boundaries = [top,bottom,left,right]
-        bat1 = bat(10,10,50,5,WHITE,screen_rect)
-        bat2 = bat(10,(screen.get_height()-10),50,5,WHITE,screen_rect)
+        bat1 = bat(10,10,50,10,WHITE,screen_rect)
+        bat2 = bat(10,(screen.get_height()-20),50,10,WHITE,screen_rect)
         circle_pos = (10+(screen.get_width()>>1),screen.get_height()>>1)
         circle_radius = 4
         ball1 = ball(screen,WHITE,circle_pos,circle_radius)
@@ -122,58 +128,34 @@ class Pong(object):
             bat1.update_stuff()
             bat2.update_stuff()
             #Code for collission detection between the ball and the walls
-            b_flag_LeftorRight= right.Rect.collidepoint(ball1.x_pos,ball1.y_pos) or left.Rect.collidepoint(ball1.x_pos,ball1.y_pos)
-            b_flag_ToporBottom= top.Rect.collidepoint(ball1.x_pos,ball1.y_pos) or bottom.Rect.collidepoint(ball1.x_pos,ball1.y_pos)
-            if(top.Rect.collidepoint(ball1.x_pos,ball1.y_pos)):
-                bat1.score -= 2
-            if(bottom.Rect.collidepoint(ball1.x_pos,ball1.y_pos)):
-                bat2.score -= 2
-            if(b_flag_LeftorRight==True):
+            b_flag_sides = (ball1.x_pos-ball1.radius <= screen_rect.left) or(ball1.x_pos+ball1.radius >= screen_rect.right)
+            b_flag_top = (ball1.y_pos - ball1.radius <=screen_rect.top)
+            b_flag_bot= (ball1.y_pos + ball1.radius >=screen_rect.bottom)
+            if(b_flag_top):
+                bat2.score += 1
+            if(b_flag_bot):
+                bat1.score += 1
+            if(b_flag_sides):
                 ball1.x_sign *=-1
-            if(b_flag_ToporBottom==True):
-                ball1.y_sign *=-1
+            if(b_flag_top or b_flag_bot):
+                ball1.reset()
             
             #Code for collission detection between the ball and the walls
 
             flag1 = bat1.Rect.collidepoint(ball1.x_pos,ball1.y_pos) 
             flag2 = bat2.Rect.collidepoint(ball1.x_pos,ball1.y_pos) 
-            if(flag1):
-                bat1.score +=1
-            if(flag2):
-               bat2.score +=1
             if(flag1 or flag2):
                ball1.y_sign *=-1
-               ball1.bounces +=1
-               if(ball1.bounces %10 == 0):
-                 ball1.speedx +=1
-                 ball1.speedy +=1
-            if(bat2.score == 1 and first_time_flag==True):
-                bat1.score = 1
-                first_time_flag = False
 
 
             ball1.draw()
             bat1.draw(screen)
             bat2.draw(screen)
             msg1 = str(bat1.score)
-            display_box(screen,msg1,(wsize-30,0))
+            display_box(screen,msg1,(wsize-40,30))
             msg2 = str(bat2.score)
-            display_box(screen,msg2,(wsize-30,hsize-20))
+            display_box(screen,msg2,(wsize-40,hsize-90))
             pygame.display.flip()
-           
-            if(bat1.score == WINNING_SCORE): 
-                screen.fill(BLACK)
-                display_box(screen,"Player 1 wins",(wsize/2,hsize/2))
-                pygame.display.flip()
-                clock1.tick(3)
-                return
-
-            if(bat2.score == WINNING_SCORE):
-                screen.fill(BLACK)
-                display_box(screen,"Player 2 wins",(wsize/2,hsize/2))
-                pygame.display.flip()
-                clock1.tick(3)
-                return
 
             clock1.tick(30)
 
@@ -189,7 +171,7 @@ class Pong(object):
 pygame.init()
 #Create a screen
 w=400 
-h=400
+h=600
 pygame.display.set_caption("Moose Pong")
 screen = pygame.display.set_mode((w,h))
 pygame.key.set_repeat(5,100)
